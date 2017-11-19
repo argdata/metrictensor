0a2462b0a9cdcc5669e19457e62239e6cdba675c
Emmanuel Contreras-Campana <emmanuelc82@gmail.com>
2017-11-18 22:01:47 -0500

    Removed unwated libraries

--- dskit/__init__.py
+++ dskit/__init__.py
@@ -1,35 +1,10 @@
-## Import common python libraries
+# Import dskit library
+from utilities import lay_out, label_encoder
 
-import sys
-import time
-import math
-import heapq
-import os.path
-import numpy as np
-import pandas as pd
-import matplotlib
-import matplotlib.pyplot as plt
-import seaborn as sns
-import random
-import operator
-import collections
+from visualization import (plot_categorical_features, plot_numerical_features,
+                           plot_correlation_matrix, plot_roc_curve,
+                           plot_precision_recall_curve, plot_learning_curve,
+                           plot_validation_curve, plot_overfitting,
+                           plot_calibration_curve, plot_confusion_matrix)
 
-# Import panda library
-import pandas.core.common as com
-from pandas.tools import plotting
-from pandas.tools.plotting import scatter_matrix
-from pandas.core.index import Index
-
-# Import scipy
-import scipy as sp
-from scipy.stats import ks_2samp
-
-# Import itertools
-import itertools
-from itertools import cycle
-
-from dskit import (lay_out, label_encoder, plot_categorical_features,
-                   plot_numerical_features, plot_correlation_matrix,
-                   plot_roc_curve, plot_precision_recall_curve, plot_learning_curve,
-                   plot_validation_curve, plot_overfitting, plot_calibration_curve,
-                   plot_confusion_matrix, nested_grid_search_cv)
+from optimization import nested_grid_search_cv
