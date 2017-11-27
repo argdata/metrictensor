f45200858df5e6a126ab957500cdef8cb8ff9f03
Christian <chrisjcc@cern.ch>
2017-11-27 16:16:34 +0100

    altered deep learing architecture setup to allow for more generic modeling

--- dskit/keras_model.py
+++ dskit/keras_model.py
@@ -7,24 +7,36 @@ from keras.regularizers import l1, l2
 
 ## Function to create model, required for KerasClassifier
 def create_model(nlayers=1, nneurons=100, dropout_rate=0.0,
-                 l2_norm=1e-3, activation='relu', kernel_initializer='uniform',
+                 l2_norm=1e-3, activation='relu', kernel_initializer='lecun_normal',
                  optimizer='adam', input_dim=None, metric='accuracy'):
 
     # create model
     model = Sequential()
 
-    # indicate the number of layers
-    for layer in range(nlayers):
-        model.add(Dense(nneurons, input_dim=input_dim,
+    # set input layer
+    model.add(Dense(nneurons,
+                        input_dim=input_dim,
                         kernel_initializer=kernel_initializer,
-                        activation=activation, kernel_regularizer=l2(l2_norm)))
+                        activation=activation,
+                        kernel_regularizer=l2(l2_norm)))
+    model.add(Dropout(dropout_rate))
+
+    # indicate the number of layers
+    for layer in range(nlayers-1):
+        model.add(Dense(nneurons, 
+                        input_dim=input_dim,
+                        kernel_initializer=kernel_initializer,
+                        activation=activation, 
+                        kernel_regularizer=l2(l2_norm)))
         model.add(Dropout(dropout_rate))
 
     # output layer
-    model.add(Dense(1, kernel_initializer=kernel_initializer,
-                    activation='sigmoid', kernel_regularizer=l2(l2_norm)))
+    model.add(Dense(1, # 2
+                    kernel_initializer=kernel_initializer,
+                    activation='sigmoid',  # activation='softmax',
+                    kernel_regularizer=l2(l2_norm)))
     # Compile model
-    model.compile(loss='binary_crossentropy', optimizer=optimizer,
+    model.compile(loss='binary_crossentropy', optimizer=Adam(lr=learning_rate), #optimizer=optimizer,
                   metrics=[metric])
 
     return model
