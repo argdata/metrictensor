33b52a4a68edeb6113c22b57d78f88fe9ca83b07
Emmanuel Contreras-Campana <emmanuelc82@gmail.com>
2017-11-28 05:12:53 -0500

    Adding statistics plot_chi2_matrix to list of available tools

--- dskit/__init__.py
+++ dskit/__init__.py
@@ -2,11 +2,13 @@
 from utilities import lay_out, label_encoder, model_grid_setup
 
 from visualization import (plot_categorical_features, plot_numerical_features,
-                           plot_correlation_matrix, plot_roc_curve,
-                           plot_precision_recall_curve, plot_learning_curve,
-                           plot_validation_curve, plot_overfitting,
-                           plot_calibration_curve, plot_confusion_matrix)
+                           plot_roc_curve, plot_precision_recall_curve,
+                           plot_learning_curve, plot_validation_curve,
+                           plot_overfitting, plot_calibration_curve,
+                           plot_confusion_matrix)
 
 from optimization import nested_grid_search_cv
+
+from statistics import plot_chi2_matrix, plot_correlation_matrix
 
 from keras_model import create_model
