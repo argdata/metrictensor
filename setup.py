95206b5410b9c4567069f4a3066987737c3f13d1
Emmanuel Contreras-Campana <emmanuelc82@gmail.com>
2017-12-15 02:20:10 -0500

    Added comments to clarify parameters in setup.py

--- setup.py
+++ setup.py
@@ -10,12 +10,12 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-setup(name='dskit',
-      version='1.0',
+setup(name='dskit', # name pip uses to uninstall, update, etc.. must be the same as package name
+      version='2.0',
       description='Data Science Toolkit',
       long_description=long_description,
       author='Emmanuel Contreras-Campana, Christian Contreras-Campana',
       author_email='emmanuelc82@gmail.com',
-      url='https://github.com/ecampana/dskit',
-      packages=['dskit']
+      url='https://github.com/ecampana/metrictensor',
+      packages=['dskit'] # directories in metrictensor to install
 )
